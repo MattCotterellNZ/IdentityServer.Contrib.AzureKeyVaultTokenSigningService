6fcad84ef2b1aac9095bf47df423a0afa1a9cb6e
Matt Cotterell <matt@cotterell.net.nz>
2015-11-01 20:08:36 +1300

    Initialize project, add class and inherited members

---
+++ src/IdentityServer.AzureKeyVaultTokenSigningService/AzureKeyVaultTokenSigningService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer3.Core.Models;
+using IdentityServer3.Core.Services;
+
+namespace IdentityServer.Contrib.AzureKeyVaultTokenSigningService
+{
+    // This project can output the Class library as a NuGet Package.
+    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+    public class AzureKeyVaultTokenSigningService : ITokenSigningService
+    {
+        public AzureKeyVaultTokenSigningService()
+        {
+        }
+
+        Task<string> ITokenSigningService.SignTokenAsync(Token token)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
