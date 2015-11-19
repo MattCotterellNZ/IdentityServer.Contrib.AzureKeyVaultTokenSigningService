2183658de78a4f81b78781dc42f756096a712535
Matt Cotterell <matt@cotterell.net.nz>
2015-11-20 00:13:07 +1300

    Rough structure created

---
+++ src/IdentityServer.AzureKeyVaultTokenSigningService/AzureKeyVaultSignatureProvider.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IdentityModel.Tokens;
+using System.Security.Cryptography;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.KeyVault.WebKey;
+
+namespace IdentityServer.AzureKeyVaultTokenSigningService
+{
+    public class AzureKeyVaultSignatureProvider : SignatureProvider
+    {
+        private readonly string _keyIdentifier;
+        private readonly string _algorithm;
+        private readonly HashAlgorithm _hashAlgorithm;
+        private readonly KeyVaultClient _keyVaultClient;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="keyIdentifier"></param>
+        /// <param name="algorithm">A valid and supported <see cref="JsonWebKeySignatureAlgorithm"/> value. Note: <see cref="JsonWebKeySignatureAlgorithm.RSNULL"/> is not supported.</param>
+        /// <param name="keyVaultAuthenticationCallback"></param>
+        /// <exception cref="ArgumentException">RSNULL is not allowed in this scenario.</exception>
+        /// <exception cref="ArgumentException">Supplied value is not a currently supported JsonWebKeySignatureAlgorithm value.</exception>
+        public AzureKeyVaultSignatureProvider(string keyIdentifier, string algorithm, KeyVaultClient.AuthenticationCallback keyVaultAuthenticationCallback) : this(keyIdentifier, algorithm, new KeyVaultClient(keyVaultAuthenticationCallback))
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="keyIdentifier"></param>
+        /// <param name="algorithm">A valid and supported <see cref="JsonWebKeySignatureAlgorithm"/> value. Note: <see cref="JsonWebKeySignatureAlgorithm.RSNULL"/> is not supported.</param>
+        /// <param name="keyVaultClient"></param>
+        /// <exception cref="ArgumentException">RSNULL is not allowed in this scenario.</exception>
+        /// <exception cref="ArgumentException">Supplied value is not a currently supported JsonWebKeySignatureAlgorithm value.</exception>
+        public AzureKeyVaultSignatureProvider(string keyIdentifier, string algorithm, KeyVaultClient keyVaultClient)
+        {
+            _keyIdentifier = keyIdentifier;
+            _algorithm = algorithm;
+            switch (algorithm)
+            {
+                case JsonWebKeySignatureAlgorithm.RS256:
+                    _hashAlgorithm = SHA256.Create();
+                    break;
+                case JsonWebKeySignatureAlgorithm.RS384:
+                    _hashAlgorithm = SHA384.Create();
+                    break;
+                case JsonWebKeySignatureAlgorithm.RS512:
+                    _hashAlgorithm = SHA512.Create();
+                    break;
+                case JsonWebKeySignatureAlgorithm.RSNULL:
+                    throw new ArgumentException("RSNULL is not allowed in this scenario.", algorithm);
+                default:
+                    throw new ArgumentException(algorithm + " is not a currently supported JsonWebKeySignatureAlgorithm value.", algorithm);
+            }
+            _keyVaultClient = keyVaultClient;
+        }
+
+        public override byte[] Sign(byte[] input)
+        {
+            var digest = _hashAlgorithm.ComputeHash(input);
+            return _keyVaultClient.SignAsync(_keyIdentifier, _algorithm, digest).Result.Result;
+        }
+
+        public override bool Verify(byte[] input, byte[] signature)
+        {
+            // TODO: If the public key is available offline (cached in memory?) verify this offline to save money and perform this operation faster
+            var digest = _hashAlgorithm.ComputeHash(input);
+            return _keyVaultClient.VerifyAsync(_keyIdentifier, _algorithm, digest, signature).Result;
+        }
+
+        protected override void Dispose(bool disposing) => Dispose();
+    }
+}
--- src/IdentityServer.AzureKeyVaultTokenSigningService/AzureKeyVaultTokenSigningService.cs
+++ src/IdentityServer.AzureKeyVaultTokenSigningService/AzureKeyVaultTokenSigningService.cs
@@ -1,23 +1,95 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IdentityModel.Tokens;
 using System.Threading.Tasks;
+using IdentityServer3.Core.Configuration;
 using IdentityServer3.Core.Models;
 using IdentityServer3.Core.Services;
+using Microsoft.Azure.KeyVault.WebKey;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using AuthenticationContext = Microsoft.IdentityModel.Clients.ActiveDirectory.AuthenticationContext;
 
-namespace IdentityServer.Contrib.AzureKeyVaultTokenSigningService
+namespace IdentityServer.AzureKeyVaultTokenSigningService
 {
     // This project can output the Class library as a NuGet Package.
     // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
     public class AzureKeyVaultTokenSigningService : ITokenSigningService
     {
-        public AzureKeyVaultTokenSigningService()
+        private readonly string _keyIdentifier;
+        private readonly string _keyVaultClientId;
+        private readonly string _keyVaultClientSecret;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AzureKeyVaultTokenSigningService"/> class.
+        /// </summary>
+        /// <param name="options">The options.</param>
+        /// <param name="keyIdentifier"></param>
+        /// <param name="keyVaultClientId"></param>
+        /// <param name="keyVaultClientSecret"></param>
+        public AzureKeyVaultTokenSigningService(string keyIdentifier, string keyVaultClientId, string keyVaultClientSecret)
         {
+            _keyIdentifier = keyIdentifier;
+            _keyVaultClientId = keyVaultClientId;
+            _keyVaultClientSecret = keyVaultClientSecret;
         }
 
-        Task<string> ITokenSigningService.SignTokenAsync(Token token)
+        /// <summary>
+        /// Signs the token.
+        /// </summary>
+        /// <param name="token">The token.</param>
+        /// <returns>
+        /// A protected and serialized security token
+        /// </returns>
+        /// <exception cref="System.InvalidOperationException">Invalid token type</exception>
+        public virtual Task<string> SignTokenAsync(Token token)
         {
-            throw new NotImplementedException();
+            return Task.FromResult(CreateJsonWebToken(token, _keyIdentifier));
+        }
+
+        /// <summary>
+        /// Creates the json web token.
+        /// </summary>
+        /// <param name="token">The token.</param>
+        /// <param name="keyIdentifier">The Azure Key Vault key identifier.</param>
+        /// <returns></returns>
+        protected virtual string CreateJsonWebToken(Token token, string keyIdentifier)
+        {
+            var jwt = new JwtSecurityToken(
+                token.Issuer,
+                token.Audience,
+                token.Claims,
+                DateTime.UtcNow,
+                DateTime.UtcNow.AddSeconds(token.Lifetime)
+            );
+
+            // amr is an array - if there is only a single value turn it into an array
+            if (jwt.Payload.ContainsKey("amr"))
+            {
+                var amrValue = jwt.Payload["amr"] as string;
+                if (amrValue != null)
+                {
+                    jwt.Payload["amr"] = new string[] { amrValue };
+                }
+            }
+
+            var rawDataBytes = System.Text.Encoding.UTF8.GetBytes(jwt.RawHeader + "." + jwt.RawPayload); // TODO: Is UTF-8 correct?
+
+            var keyVaultSignatureProvider = new AzureKeyVaultSignatureProvider(keyIdentifier, JsonWebKeySignatureAlgorithm.RS256, KeyVaultClientSecretAuthenticationCallback);
+
+            var rawSignature = Convert.ToBase64String(keyVaultSignatureProvider.Sign(rawDataBytes));
+
+            return jwt.RawHeader + "." + jwt.RawPayload + "." + rawSignature;
+        }
+
+        private async Task<string> KeyVaultClientSecretAuthenticationCallback(string authority, string resource, string scope)
+        {
+            var authContext = new AuthenticationContext(authority);
+            ClientCredential clientCred = new ClientCredential(_keyVaultClientId, _keyVaultClientSecret);
+            AuthenticationResult result = await authContext.AcquireTokenAsync(resource, clientCred);
+
+            if (result == null)
+                throw new InvalidOperationException("Failed to obtain the JWT token");
+
+            return result.AccessToken;
         }
     }
 }
