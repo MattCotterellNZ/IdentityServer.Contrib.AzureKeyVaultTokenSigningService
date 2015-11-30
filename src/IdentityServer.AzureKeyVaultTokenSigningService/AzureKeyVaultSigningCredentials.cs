10463ed8c8f1e06d4c651d6375ee4755054d02cf
Matt Cotterell <matt@cotterell.net.nz>
2015-11-30 22:05:40 +1300

    Fix "alg": "none" glitch based off IdentityServer3 2.2 source

---
+++ src/IdentityServer.AzureKeyVaultTokenSigningService/AzureKeyVaultSigningCredentials.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IdentityModel.Tokens;
+
+namespace IdentityServer.AzureKeyVaultTokenSigningService
+{
+    public class AzureKeyVaultSigningCredentials : SigningCredentials
+    {
+        public AzureKeyVaultSigningCredentials(RsaSecurityKey signingKey, string digestAlgorithm, SecurityKeyIdentifier signingKeyIdentifier = null) : base(signingKey, SecurityAlgorithms.RsaSha256Signature, digestAlgorithm, signingKeyIdentifier)
+        {
+            if (signingKey.HasPrivateKey())
+            {
+                throw new ArgumentException("For security reasons, the signing key cannot contain the private key. Please remove all traces of this from the application and defer to Azure Key Vault for signing.", nameof(signingKey));
+            }
+
+            if (digestAlgorithm != SecurityAlgorithms.Sha256Digest && digestAlgorithm != SecurityAlgorithms.Sha512Digest)
+            {
+                throw new ArgumentOutOfRangeException(nameof(digestAlgorithm), digestAlgorithm, "Only SHA256 and SHA512 are supported at this time.");
+            }
+        }
+    }
+}
--- src/IdentityServer.AzureKeyVaultTokenSigningService/AzureKeyVaultTokenSigningService.cs
+++ src/IdentityServer.AzureKeyVaultTokenSigningService/AzureKeyVaultTokenSigningService.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IdentityModel.Tokens;
 using System.Threading.Tasks;
-using IdentityServer3.Core.Configuration;
+using IdentityServer3.Core;
 using IdentityServer3.Core.Models;
 using IdentityServer3.Core.Services;
-using Microsoft.Azure.KeyVault.WebKey;
 using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Newtonsoft.Json.Linq;
 using AuthenticationContext = Microsoft.IdentityModel.Clients.ActiveDirectory.AuthenticationContext;
+using System.Linq;
+using System.Security.Cryptography;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.KeyVault.WebKey;
 
 namespace IdentityServer.AzureKeyVaultTokenSigningService
 {
@@ -17,6 +22,8 @@ namespace IdentityServer.AzureKeyVaultTokenSigningService
         private readonly string _keyIdentifier;
         private readonly string _keyVaultClientId;
         private readonly string _keyVaultClientSecret;
+        private byte[] _keyVaultKeyExponent;
+        private byte[] _keyVaultKeyModulus;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="AzureKeyVaultTokenSigningService"/> class.
@@ -39,48 +46,175 @@ namespace IdentityServer.AzureKeyVaultTokenSigningService
         /// <returns>
         /// A protected and serialized security token
         /// </returns>
-        /// <exception cref="System.InvalidOperationException">Invalid token type</exception>
-        public virtual Task<string> SignTokenAsync(Token token)
+        public virtual async Task<string> SignTokenAsync(Token token)
         {
-            return Task.FromResult(CreateJsonWebToken(token, _keyIdentifier));
+            var credentials = await GetSigningCredentialsAsync();
+            return await CreateJsonWebToken(token, credentials);
+        }
+
+        /// <summary>
+        /// Retrieves the signing credential (override to load key from alternative locations)
+        /// </summary>
+        /// <returns>The signing credential</returns>
+        protected virtual async Task<AzureKeyVaultSigningCredentials> GetSigningCredentialsAsync()
+        {
+            if (_keyVaultKeyExponent == null && _keyVaultKeyModulus == null)
+            {
+                var keyVaultClient = new KeyVaultClient(KeyVaultClientAuthenticationCallback);
+                var keyBundle = await keyVaultClient.GetKeyAsync(_keyIdentifier).ConfigureAwait(false);
+                
+                _keyVaultKeyExponent = keyBundle.Key.E;
+                _keyVaultKeyModulus = keyBundle.Key.N;
+            }
+
+            var rsa = RSA.Create();
+            rsa.ImportParameters(new RSAParameters
+            {
+                Exponent = _keyVaultKeyExponent,
+                Modulus = _keyVaultKeyModulus,
+            });
+
+            var securityKey = new RsaSecurityKey(rsa);
+            return new AzureKeyVaultSigningCredentials(securityKey, SecurityAlgorithms.Sha256Digest);
         }
 
         /// <summary>
         /// Creates the json web token.
         /// </summary>
         /// <param name="token">The token.</param>
-        /// <param name="keyIdentifier">The Azure Key Vault key identifier.</param>
-        /// <returns></returns>
-        protected virtual string CreateJsonWebToken(Token token, string keyIdentifier)
+        /// <param name="credentials">The credentials.</param>
+        /// <returns>The signed JWT</returns>
+        protected virtual async Task<string> CreateJsonWebToken(Token token, AzureKeyVaultSigningCredentials credentials)
         {
-            var jwt = new JwtSecurityToken(
+            var header = CreateHeader(token, credentials);
+            var payload = CreatePayload(token);
+
+            return await SignAsync(new JwtSecurityToken(header, payload));
+        }
+
+        /// <summary>
+        /// Creates the JWT header
+        /// </summary>
+        /// <param name="token">The token.</param>
+        /// <param name="keyVaultCredentials">The credentials.</param>
+        /// <returns>The JWT header</returns>
+        protected virtual JwtHeader CreateHeader(Token token, AzureKeyVaultSigningCredentials keyVaultCredentials)
+        {
+            var header = new JwtHeader(keyVaultCredentials);
+            if (keyVaultCredentials != null)
+            {
+                header.Add("kid", _keyIdentifier);
+            }
+
+            return header;
+        }
+
+        /// <summary>
+        /// Creates the JWT payload
+        /// </summary>
+        /// <param name="token">The token.</param>
+        /// <returns>The JWT payload</returns>
+        protected virtual JwtPayload CreatePayload(Token token)
+        {
+            var payload = new JwtPayload(
                 token.Issuer,
                 token.Audience,
-                token.Claims,
+                null,
                 DateTime.UtcNow,
-                DateTime.UtcNow.AddSeconds(token.Lifetime)
-            );
+                DateTime.UtcNow.AddSeconds(token.Lifetime));
 
-            // amr is an array - if there is only a single value turn it into an array
-            if (jwt.Payload.ContainsKey("amr"))
+            var amrClaims = token.Claims.Where(x => x.Type == Constants.ClaimTypes.AuthenticationMethod);
+            var jsonClaims = token.Claims.Where(x => x.ValueType == Constants.ClaimValueTypes.Json);
+            var normalClaims = token.Claims.Except(amrClaims).Except(jsonClaims);
+
+            payload.AddClaims(normalClaims);
+
+            // deal with amr
+            var amrValues = amrClaims.Select(x => x.Value).Distinct().ToArray();
+            if (amrValues.Any())
             {
-                var amrValue = jwt.Payload["amr"] as string;
-                if (amrValue != null)
+                payload.Add(Constants.ClaimTypes.AuthenticationMethod, amrValues);
+            }
+
+            // deal with json types
+            // calling ToArray() to trigger JSON parsing once and so later 
+            // collection identity comparisons work for the anonymous type
+            var jsonTokens = jsonClaims.Select(x => new { x.Type, JsonValue = JRaw.Parse(x.Value) }).ToArray();
+
+            var jsonObjects = jsonTokens.Where(x => x.JsonValue.Type == JTokenType.Object).ToArray();
+            var jsonObjectGroups = jsonObjects.GroupBy(x => x.Type).ToArray();
+            foreach (var group in jsonObjectGroups)
+            {
+                if (payload.ContainsKey(group.Key))
                 {
-                    jwt.Payload["amr"] = new string[] { amrValue };
+                    throw new Exception(String.Format("Can't add two claims where one is a JSON object and the other is not a JSON object ({0})", group.Key));
+                }
+
+                if (group.Skip(1).Any())
+                {
+                    // add as array
+                    payload.Add(group.Key, group.Select(x => x.JsonValue).ToArray());
+                }
+                else
+                {
+                    // add just one
+                    payload.Add(group.Key, group.First().JsonValue);
                 }
             }
 
-            var rawDataBytes = System.Text.Encoding.UTF8.GetBytes(jwt.EncodedHeader + "." + jwt.EncodedPayload); // TODO: Is UTF-8 correct?
+            var jsonArrays = jsonTokens.Where(x => x.JsonValue.Type == JTokenType.Array).ToArray();
+            var jsonArrayGroups = jsonArrays.GroupBy(x => x.Type).ToArray();
+            foreach (var group in jsonArrayGroups)
+            {
+                if (payload.ContainsKey(group.Key))
+                {
+                    throw new Exception(String.Format("Can't add two claims where one is a JSON array and the other is not a JSON array ({0})", group.Key));
+                }
 
-            var keyVaultSignatureProvider = new AzureKeyVaultSignatureProvider(keyIdentifier, JsonWebKeySignatureAlgorithm.RS256, KeyVaultClientSecretAuthenticationCallback);
+                List<JToken> newArr = new List<JToken>();
+                foreach (var arrays in group)
+                {
+                    var arr = (JArray)arrays.JsonValue;
+                    newArr.AddRange(arr);
+                }
 
-            var rawSignature = Convert.ToBase64String(keyVaultSignatureProvider.Sign(rawDataBytes));
+                // add just one array for the group/key/claim type
+                payload.Add(group.Key, newArr.ToArray());
+            }
 
-            return jwt.EncodedHeader + "." + jwt.EncodedPayload + "." + rawSignature;
+            var unsupportedJsonTokens = jsonTokens.Except(jsonObjects).Except(jsonArrays);
+            var unsupportedJsonClaimTypes = unsupportedJsonTokens.Select(x => x.Type).Distinct();
+            if (unsupportedJsonClaimTypes.Any())
+            {
+                throw new Exception(String.Format("Unsupported JSON type for claim types: {0}", unsupportedJsonClaimTypes.Aggregate((x, y) => x + ", " + y)));
+            }
+
+            return payload;
         }
 
-        private async Task<string> KeyVaultClientSecretAuthenticationCallback(string authority, string resource, string scope)
+        /// <summary>
+        /// Applies the signature to the JWT
+        /// </summary>
+        /// <param name="jwt">The JWT object.</param>
+        /// <returns>The signed JWT</returns>
+        protected virtual async Task<string> SignAsync(JwtSecurityToken jwt)
+        {
+            var rawDataBytes = System.Text.Encoding.UTF8.GetBytes(jwt.EncodedHeader + "." + jwt.EncodedPayload); // TODO: Is UTF-8 correct?
+
+            var keyVaultSignatureProvider = new AzureKeyVaultSignatureProvider(_keyIdentifier, JsonWebKeySignatureAlgorithm.RS256, KeyVaultClientAuthenticationCallback);
+
+            var rawSignature = await Task.Run(() => Convert.ToBase64String(keyVaultSignatureProvider.Sign(rawDataBytes))).ConfigureAwait(false);
+
+            return jwt.EncodedHeader + "." + jwt.EncodedPayload + "." + rawSignature;
+
+            //var handler = new JwtSecurityTokenHandler
+            //{
+            //    SignatureProviderFactory = new AzureKeyVaultSignatureProviderFactory()
+            //};
+            //return Task.FromResult(handler.WriteToken(jwt));
+        }
+
+        private async Task<string> KeyVaultClientAuthenticationCallback(string authority, string resource, string scope)
         {
             var authContext = new AuthenticationContext(authority);
             ClientCredential clientCred = new ClientCredential(_keyVaultClientId, _keyVaultClientSecret);
